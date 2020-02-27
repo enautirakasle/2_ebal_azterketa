052842d6f3fb1873dcc92aa0d6252bbf4b4bb8ca
Eñaut Agirre <enaut.irakasle@gmail.com>
2020-02-27 22:15:18 +0100

    simplificacion del algoritmo

--- src/ejecutables/ConsultarInscripciones.java
+++ src/ejecutables/ConsultarInscripciones.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 import modelo.bean.Actividad;
 import modelo.bean.Inscripcion;
+import modelo.bean.Usuario;
 import modelo.dao.ModeloActividad;
 import modelo.dao.ModeloInscripcion;
 
@@ -16,16 +17,16 @@ public class ConsultarInscripciones {
 		System.out.println("Introduce un id de actividad");
 		int idActividad = Integer.parseInt(scan.nextLine());
 		
-		ModeloActividad mActividad = new ModeloActividad();
+		ModeloInscripcion mInscripcion = new ModeloInscripcion();
 
-		Actividad actividad = mActividad.getConInscipciones(idActividad);
-		ArrayList<Inscripcion> inscripciones = actividad.getIscripciones();
+		ArrayList<Usuario> usuariosInscritos = mInscripcion.getUsuariosInscritos(idActividad);
 		
 		System.out.println("Listado de usuarios inscriptos");
-		for(int i = 0; i<inscripciones.size(); i++){
-			Inscripcion inscripcion = inscripciones.get(i);
+		
+		for(int i = 0; i<usuariosInscritos.size(); i++){
+			Usuario usuario = usuariosInscritos.get(i);
 			
-			System.out.println(inscripcion.getUsuario());
+			System.out.println(usuario);
 		}
 		
 	}
--- src/modelo/dao/ModeloInscripcion.java
+++ src/modelo/dao/ModeloInscripcion.java
@@ -1,10 +1,13 @@
 package modelo.dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import modelo.Conector;
 import modelo.bean.Inscripcion;
+import modelo.bean.Usuario;
 
 public class ModeloInscripcion extends Conector{
 
@@ -19,6 +22,36 @@ public class ModeloInscripcion extends Conector{
 			e.printStackTrace();
 		}
 		
+		
+	}
+
+	public ArrayList<Usuario> getUsuariosInscritos(int idActividad) {
+		
+		ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+		
+		try {
+			PreparedStatement pst = super.conexion.prepareStatement("select usuarios.* "
+					+ "from inscripciones join usuarios on inscripciones.id_usuario= usuarios.id "
+					+ "where inscripciones.id_actividad = ?");
+			pst.setInt(1, idActividad);
+
+			ResultSet rs = pst.executeQuery();
+			
+			
+			while(rs.next()){
+				Usuario usuario = new Usuario();
+				usuario.setId(rs.getInt("id"));
+				usuario.setNombreApellido(rs.getString("nombre_apellido"));
+				usuario.setDni(rs.getString("dni"));
+				usuario.setCodigo(rs.getString("codigo"));
+				
+				usuarios.add(usuario);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return usuarios;
 		
 	}
 	
